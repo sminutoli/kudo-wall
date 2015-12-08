a6e66e759553a13044ffbf6143d29680a275b44e
Florius <j@florius.com.ar>
2015-12-08 10:30:58 -0300

    mas tests del parser

--- helpers/interpretar.js
+++ helpers/interpretar.js
@@ -1,12 +1,12 @@
 module.exports =  function(body){
     var mensaje = body.match(/a (.*) por (.*)/i);
     if(mensaje === null || mensaje.length != 3){
-        throw 'El formato del Kudo debe ser: `/kudo para [alguien] por [algo]`.';
+        throw new Error('El formato del Kudo debe ser: `/kudo para [alguien] por [algo]`.');
     } 
 
     return {
         para: mensaje[1],
-            por: mensaje[2]
+        por: mensaje[2]
     };
 };
 
--- test/separador_test.js
+++ test/separador_test.js
@@ -10,4 +10,26 @@ describe('Interpretador', function(){
 
         done();
     })
+    it('debería tirar un error si el mensaje no tiene nada de sentido', function(done){
+        expect(function(){
+            interpretar('Interpretame. I dare you!');
+        }).to.throwError(/formato/i);
+        
+        done();
+    })
+    it('debería tirar un error si el mensaje no tiene todo el sentido', function(done){
+        expect(function(){
+            interpretar('para Interpretame');
+        }).to.throwError(/formato/i);
+        
+        done();
+    })
+    it('debería aceptar tambien el formato con la "a" en vez del "para"', function(done){
+        var iterpretado = interpretar('a mi por ser tan groso');
+
+        expect( iterpretado  ).to.have.property('para', 'mi');
+        expect( iterpretado  ).to.have.property('por', 'ser tan groso');
+
+        done();
+    })
 });
