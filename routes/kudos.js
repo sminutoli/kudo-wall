0820ed3435a238bd96131eafae94e7e8cbaf3906
Florius <j@florius.com.ar>
2017-03-10 13:40:05 -0300

    log el kudo creado

--- routes/kudos.js
+++ routes/kudos.js
@@ -32,6 +32,7 @@ router.post('/nuevo', function(req, res, next) {
 
   try{
     Kudo.armar( req.body.text, req.body.user_name, function(kudoCreado){
+      console.log(kudoCreado);
       res.send({
               "text": "Gracias por dejar tu Kudo!",
               "attachments": [{
