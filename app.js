a3d3eb196a8cf814ff3756b0dd811304b6de3380
Florius <j@florius.com.ar>
2017-11-10 16:07:54 -0300

    levantar el express tambien como infrastructra

--- app.js
+++ app.js
@@ -1,53 +1,14 @@
-var express = require('express')
-  , kudos = require('./routes/kudos')
-  , db = require('./server/db')
-  , http = require('http')
-  , path = require('path')
-  , exphbs  = require('express-handlebars')
-  , hbshelpers = require('./lib/handlebarsHelpers');
+var db = require('./server/db')
+  , app = require('./server/rest')
+  , bluebird = require('bluebird');
 
-var favicon = require('serve-favicon')
-  , logger = require('morgan')
-  , connect        = require('connect')
-  , methodOverride = require('method-override')
-  , bodyParser = require('body-parser')
-  , multer = require('multer')
-  , errorHandler = require('errorhandler')
-  , config = require('./config');
-
-
-db.start().then(() => {
-  var app = express();
-
-  // all environments
-  app.set('port', config.application.port);
-  app.set('views', path.join(__dirname, 'views'));
-  app.engine('handlebars', exphbs({
-                              defaultLayout: 'main',
-                              helpers: hbshelpers
-                          }));
-  app.set('view engine', 'handlebars');
-  app.use(favicon(__dirname + '/public/favicon.ico'));
-  app.use(logger('dev'));
-  app.use(methodOverride('_method'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(multer());
-  app.use(express.static(path.join(__dirname, 'public')));
-
-
-  app.use('/', kudos);
-
-  // development only
-  if ('development' == app.get('env')) {
-    app.use(errorHandler());
-  }
-
-  var server = http.createServer(app);
-  server.listen(config.application.port, function(){
-    console.log('Express server listening on port ' + config.application.port);
-  });
-}).catch(err => {
+bluebird.resolve()
+.then(
+  db.start
+).then(
+  app.start
+)
+.catch(err => {
     console.error('App starting error:', err.stack);
     process.exit(1);
 });
---
+++ server/rest.js
@@ -0,0 +1,48 @@
+var express = require('express')
+  , kudos = require('../routes/kudos')
+  , http = require('http')
+  , path = require('path')
+  , exphbs  = require('express-handlebars')
+  , hbshelpers = require('../lib/handlebarsHelpers')
+  , favicon = require('serve-favicon')
+  , logger = require('morgan')
+  , connect        = require('connect')
+  , methodOverride = require('method-override')
+  , bodyParser = require('body-parser')
+  , multer = require('multer')
+  , errorHandler = require('errorhandler')
+  , config = require('../config');
+
+module.exports.start = function(){
+    var app = express();
+
+    // all environments
+    app.set('port', config.application.port);
+    app.set('views', path.join(__dirname, 'views'));
+    app.engine('handlebars', exphbs({
+                                defaultLayout: 'main',
+                                helpers: hbshelpers
+                            }));
+    app.set('view engine', 'handlebars');
+    app.use(favicon(__dirname + '/../public/favicon.ico'));
+    app.use(logger('dev'));
+    app.use(methodOverride('_method'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(multer());
+    app.use(express.static(path.join(__dirname, 'public')));
+
+
+    app.use('/', kudos);
+
+    // development only
+    if ('development' == app.get('env')) {
+      app.use(errorHandler());
+    }
+
+    var server = http.createServer(app);
+
+  server.listen(config.application.port)
+  console.log('Express server listening on port ' + config.application.port);
+  return Promise.resolve(true);  
+}
