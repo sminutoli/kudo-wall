2f412e3c3472be1f0b4fadcc25cb01b6403d162d
lalo <lalopsb@hotmail.com>
2020-05-15 17:03:26 -0300

    Incluir unescape de entidades html en sanitizador

--- app/public/javascripts/setup.js
+++ app/public/javascripts/setup.js
@@ -1,9 +1,24 @@
 var kudos = [];
+
 function dibujarKudo(id, texto, para, autor){
     kudos.push({
         id: id,
-        texto: texto,
+        texto: decodeEntities(texto),
         autor: autor,
-        para: para
+        para: decodeEntities(para)
     });
 }
+
+var decodeEntities = (function() {
+    var element = document.createElement('div');
+    function decodeHTMLEntities (str) {
+        if(str && typeof str === 'string') {
+            str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
+            element.innerHTML = str;
+            str = element.textContent;
+            element.textContent = '';
+        }
+        return str;
+    }
+    return decodeHTMLEntities;
+})();
