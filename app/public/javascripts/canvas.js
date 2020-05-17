fe25d081ca6be2be5d667c34bfce66371e1c4d4d
lalo <lalopsb@hotmail.com>
2020-05-17 20:32:20 -0300

    mejoras visuales en pos de legibilidad

--- app/public/javascripts/canvas.js
+++ app/public/javascripts/canvas.js
@@ -1,6 +1,6 @@
 //Preparar los fonts y artes
 var f = new FontFace('OpenDyslexic', 'url(\'/font/OpenDyslexic-Regular.otf\')').load();
-var r = load('/imagenes/renglon.jpg');
+var r = load('/imagenes/renglongris.png');
 for(var i,l=[i=0];i<5;l[i++]=i);
 
 var ls = Promise.all(
@@ -12,7 +12,8 @@ var ls = Promise.all(
 //Valores comunes
 var maxLogoWidth = 200;
 var headerHeight = 70;
-var padding = 5;
+var lineHeight = 36;
+var padding = 15;
 
 Promise.all([f, r, ls]).then(function(values){
     var font = values[0];
@@ -26,7 +27,7 @@ Promise.all([f, r, ls]).then(function(values){
         var ctx = canvas.getContext("2d");
 
         //Elejir color y logo
-        var color = '#' + Math.floor(Math.random()*16777215).toString(16);
+        var color = "hsl(" + Math.random() * 360 + ", 100%, 75%)";
         var logo = logos[Math.floor(Math.random() * logos.length)];
 
         //Dibujar header
@@ -39,8 +40,8 @@ Promise.all([f, r, ls]).then(function(values){
         ctx.fillRect(0, headerHeight, canvas.width, canvas.height);
 
         //Linita del header
-        ctx.setLineDash([40, 25]);
-        ctx.lineWidth=10;
+        ctx.setLineDash([20, 25]);
+        ctx.lineWidth=3;
         ctx.strokeRect(0, headerHeight, canvas.width, 0);
 
         //Dibujar logo
@@ -55,21 +56,25 @@ Promise.all([f, r, ls]).then(function(values){
         ctx.textBaseline = 'top';
 
         //Dibujar el texto al rededor
-        var kudoText = kudo.texto + "\t\t --" + kudo.autor;
-        wrapText(ctx, kudoText, padding, headerHeight, logo.width*ratio, logo.height*ratio+headerHeight, canvas.width-padding, 36);
+        var kudoText = kudo.texto;
+        var wrapHeight = wrapText(ctx, kudoText, padding, headerHeight+lineHeight, logo.width*ratio, logo.height*ratio+headerHeight, canvas.width-padding, lineHeight);
 
         //Texto header
         ctx.strokeStyle = "white";
         var kudoPara = "Kudos para " + kudo.para;
-        ctx.strokeText(kudoPara, padding, 4);
-        ctx.fillText(kudoPara, padding, 4);
-        
+        ctx.strokeText(kudoPara, padding, 20);
+        ctx.fillText(kudoPara, padding, 20);
+
         //Borde al rededor
         ctx.strokeStyle = "black";
         ctx.setLineDash([]);
         ctx.lineWidth=4;
         ctx.fillStyle=color;
         ctx.strokeRect(0,0,canvas.width,canvas.height);
+        //Autor dejando un reglón vacío
+        ctx.font= '24px ' + font.family;
+        ctx.fillStyle = 'black';
+        ctx.fillText(" --" + kudo.autor, padding, wrapHeight+ 2*lineHeight);
     });
 });
 
@@ -94,13 +99,14 @@ function wrapText(ctx, texto, x, y, bbx, bby, maxWidth, lineHeight) {
         }
     }
     ctx.fillText(line, x, y);
+    return y;
 }
 
 function load(url) {
     return new Promise(function(resolve, reject) {
         var img = new Image();
         img.src = url;
-        
+
         img.onload = function() {
             resolve(img);
         };
